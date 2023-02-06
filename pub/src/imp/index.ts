c42776b37e15ab3d6b969785910fe6f9ca981f7a
corno <corno.astn.a@schraverus.com>
2023-02-06 18:55:14 +0100

    .

--- pub/src/imp/index.ts
+++
@@ -1,3 +0,0 @@
-
-// export { createExpectContext } from "./createExpectContext.p"
-export { createExpectIssueMessage } from "./createExpectIssueMessage.p"
--- pub/src/interface/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./interfaces/IExpectContext.p"
-export * from "./types/ExpectIssue.p"
-export * from "./types/ExpectedValue.p"
-
-export * from "./tbd.p"
