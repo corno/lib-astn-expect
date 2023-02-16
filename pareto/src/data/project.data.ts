79c2dc1982e5bbb92c33c9a393d675032afa7533
corno <corno.astn.a@schraverus.com>
2023-02-16 19:47:08 +0100

    .

--- pareto/src/data/project.data.ts
+++ pareto/src/data/project.data.ts
@@ -11,7 +11,8 @@ export const $: mproject.T.Project = {
     'description': "create handlers that will validate that the received data is according to expectations",
     'license': "ISC",
 
-    'pubdependencies': d({
+    'dependencies': d({
+        "lib-astn-dummyhandlers": {},
     }),
     'type': ['library', {
         'main': {
@@ -23,6 +24,13 @@ export const $: mproject.T.Project = {
         'test': {
             'dependencies': d({
             }),
+            'glossary': {
+                'functions': d({}),
+                'imports': d({}),
+                'parameters': d({}),
+                'types': d({}),
+                'interfaces': d({}),
+            },
         }
     }],
 }
--- test/src/old/implementation/index.ts
+++
@@ -1,4 +0,0 @@
-import * as pt from 'pareto-core-types'
-// export * from "./test.p"
-// export * from "./createTestContext"
-// export * from "./dummyHandlers"
